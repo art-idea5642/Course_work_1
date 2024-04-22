819e140648af591160089c7261b962f8137bca07
art-idea5642 <chertezhy.ru@gmail.com>
2024-04-22 15:44:26 +0400

    Hard difficulty

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import EmployeeBook.EmployeeBook;
 public class Main {
 
     public static void main(String[] args) {
-        EmployeeBook employeeBook = new EmployeeBook();
+        EmployeeBook employeeBook = new EmployeeBook(35);
         employeeBook.add("Иванова Мария Петровна", 1, 125000);
         employeeBook.add("Смирнов Алексей Владимирович", 3, 83000);
         employeeBook.add("Попова Екатерина Андреевна", 5, 140000);
@@ -14,8 +14,30 @@ public class Main {
         employeeBook.add("Волкова Ольга Дмитриевна", 2, 72000);
         employeeBook.printAll();
         employeeBook.deleteById(4);
+        System.out.println();
         employeeBook.printAll();
+        System.out.println();
         System.out.println(employeeBook.getEmployeeById(6));
-
+        System.out.println(employeeBook.getSum());
+        System.out.println(employeeBook.getMinSalary());
+        System.out.println(employeeBook.getMaxSalary());
+        System.out.println(employeeBook.getAverageSalary());
+        System.out.println();
+        employeeBook.printAllNames();
+        employeeBook.increaseSalaryByPercentage(20);
+        System.out.println();
+        employeeBook.printAll();
+        System.out.println();
+        System.out.println(employeeBook.getMinSalary(1));
+        System.out.println(employeeBook.getMaxSalary(1));
+        System.out.println(employeeBook.getSum(1));
+        System.out.println(employeeBook.getAverageSalary(1));
+        employeeBook.increaseSalaryByPercentage(2, 30);
+        System.out.println();
+        employeeBook.printAll();
+        System.out.println();
+        employeeBook.printAllWithLessSalary(130000);
+        System.out.println();
+        employeeBook.printAllWithMoreSalary(130000);
     }
 }
